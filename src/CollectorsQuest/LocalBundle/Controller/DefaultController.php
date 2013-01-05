6d4435097867cf4eb8a35c14fcc1327c9319f629
Kiril Angov <kiril.angov@gmail.com>
2013-01-05 15:31:47 -0500

    First steps to interact with the Foursquare API

--- src/CollectorsQuest/LocalBundle/Controller/DefaultController.php
+++ src/CollectorsQuest/LocalBundle/Controller/DefaultController.php
@@ -3,12 +3,58 @@
 namespace CollectorsQuest\LocalBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TheTwelve\Foursquare;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('CollectorsQuestLocalBundle:Default:index.html.twig');
+    }
+
+    public function connectAction()
+    {
+        $client = new Foursquare\HttpClient\SymfonyHttpClient();
+
+        $factory = new Foursquare\ApiGatewayFactory($client);
+        $factory->setEndpointUri('https://api.foursquare.com');
+        $factory->useVersion(2);
+
+        $auth = $factory->getAuthenticationGateway(
+            'MWPCILL1FDZVMWDNFFM2DBZPXA1LE0HB5LWG02EFM0MSBGX4',
+            'OBNVHOT0RSK2K5E5BHDFI4JZOWYYNK0U5CZHIXL0L3ZM4BF2',
+            'https://foursquare.com/oauth2/authorize',
+            'https://foursquare.com/oauth2/access_token',
+            'http://local.collectorsquest.dev/app.php/callback'
+        );
+
+        return $auth->initiateLogin();
+    }
+
+    public function callbackAction()
+    {
+        $client = new Foursquare\HttpClient\SymfonyHttpClient();
+
+        $factory = new Foursquare\ApiGatewayFactory($client);
+        $factory->setEndpointUri('https://api.foursquare.com');
+        $factory->useVersion(2);
+
+        $auth = $factory->getAuthenticationGateway(
+            'MWPCILL1FDZVMWDNFFM2DBZPXA1LE0HB5LWG02EFM0MSBGX4',
+            'OBNVHOT0RSK2K5E5BHDFI4JZOWYYNK0U5CZHIXL0L3ZM4BF2',
+            'https://foursquare.com/oauth2/authorize',
+            'https://foursquare.com/oauth2/access_token',
+            'http://local.collectorsquest.dev/app.php/callback'
+        );
+
+        $token = $auth->authenticateUser($_GET['code']);
+
+        $factory->setToken($token);
+        $gateway = $factory->getUsersGateway();
+        $user = $gateway->getUser();
+
+        $user->
+        print_r($user); exit;
     }
 
     public function privacyAction()
