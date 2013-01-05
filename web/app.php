0d7c6cc8c31899049beaaa95fd6da44bf93df629
Kiril Angov <kiril.angov@gmail.com>
2013-01-05 12:08:44 -0500

    Combine app.php and app_dev.php into one file app.php

--- web/app.php
+++ web/app.php
@@ -4,21 +4,30 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
-// Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-$loader = new ApcClassLoader('cq', $loader);
-$loader->register(true);
-
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+if (in_array(@$_SERVER['REMOTE_ADDR'], array('172.16.183.1', '::1',))) {
+
+    $kernel = new AppKernel('dev', true);
+    $kernel->loadClassCache();
+
+} else {
+
+    require_once __DIR__.'/../app/AppCache.php';
+
+    // Use APC for autoloading to improve performance
+    // Change 'sf2' by the prefix you want in order to prevent key conflict with another application
+    $loader = new ApcClassLoader('cq', $loader);
+    $loader->register(true);
+
+    $kernel = new AppKernel('prod', false);
+    $kernel->loadClassCache();
+    $kernel = new AppCache($kernel);
+}
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 
 $kernel->terminate($request, $response);
+
